eaa615ecb2d1e0e472e07c7eaa049ce58fa966c9
Jason Miller <contact@jasonmiller.nl>
2021-12-19 18:27:24 +0100

    fix: add nvim/vim bindings

--- config/nvim/lua/utils/bindings.lua
+++ config/nvim/lua/utils/bindings.lua
@@ -1,2 +1,7 @@
 -- Rebind functions to shorter names
 o = vim.o		-- Vim option
+g = vim.g		-- Vim global option
+b = vim.b
+wo = vim.wo		-- Vim window option
+bo = vim.bo		-- Vim buffer option
+fn = vim.fn		-- Vim function
