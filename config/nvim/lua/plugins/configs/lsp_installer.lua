cc1c5f67d16f79e4d02e03d9d4d2234e6ac190f5
Jason Miller <contact@jasonmiller.nl>
2021-12-19 22:34:17 +0100

    fix(nvim): lsp server install

--- config/nvim/lua/plugins/configs/lsp_installer.lua
+++ config/nvim/lua/plugins/configs/lsp_installer.lua
@@ -1,14 +1,39 @@
-local lsp_installer_servers = require'nvim-lsp-installer.servers'
+local lsp_installer = require('nvim-lsp-installer')
 
-local server_available, requested_server = lsp_installer_servers.get_server("rust_analyzer")
+-- Include the servers you want to have installed by default below
+local servers = {
+  "rust_analyzer",
+  "vuels",
+}
 
-if server_available then
+for _, name in pairs(servers) do
+  local server_available, requested_server = lsp_installer.get_server(name)
+
+  if server_available then
     requested_server:on_ready(function ()
         local opts = {}
         requested_server:setup(opts)
     end)
+
     if not requested_server:is_installed() then
-        -- Queue the server to be installed
-        requested_server:install()
+      -- Queue the server to be installed
+      print("Installing " .. name)
+      requested_server:install()
     end
+  end
 end
+
+-- local lsp_installer_servers = require'nvim-lsp-installer.servers'
+--
+-- local server_available, requested_server = lsp_installer_servers.get_server("rust_analyzer")
+--
+-- if server_available then
+--     requested_server:on_ready(function ()
+--         local opts = {}
+--         requested_server:setup(opts)
+--     end)
+--     if not requested_server:is_installed() then
+--         -- Queue the server to be installed
+--         requested_server:install()
+--     end
+-- end
