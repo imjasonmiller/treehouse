f338543dc9d69d5e56474c7f49d3b35e923090c9
Jason Miller <contact@jasonmiller.nl>
2022-01-15 12:01:46 +0100

    feat(ssh): start agent on login

--- nixos/trait/networking.nix
+++ nixos/trait/networking.nix
@@ -1,9 +1,11 @@
 { config, ... }:
 {
   programs.gnupg.agent = {
-    enable = true;
+    enable = false;
     enableSSHSupport = true;
   };
+
+  programs.ssh.startAgent = true;
 
   services.openssh = {
     enable = true;
