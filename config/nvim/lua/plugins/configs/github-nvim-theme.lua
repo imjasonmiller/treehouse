7a32ccb46e73681ebb5f8a4ac229fa82833151fe
Jason Miller <contact@jasonmiller.nl>
2022-01-19 23:39:04 +0100

    fix(nvim): override colors for github nvim theme

--- config/nvim/lua/plugins/configs/github-nvim-theme.lua
+++ config/nvim/lua/plugins/configs/github-nvim-theme.lua
@@ -3,9 +3,13 @@ require("github-theme").setup({
   sidebars = { "terminal", "packer" },
   overrides = function(c)
     return {
-      LineNr = { fg = c.bg_visual },
-      NonText = { fg = c.bg_visual },
-      ColorColumn = { bg = c.bg_visual },
+      LineNr = { fg = '#484F58' },
+      -- Change the color for indent blankline
+      NonText = { fg = '#161B22' },
+      ColorColumn = { bg = '#161B22' },
+      GitSignsAdd = { fg = '#238636' },
+      GitSignsChange = { fg = '#8957E5' },
+      GitSignsDelete = { fg = '#DA3633' },
     }
   end
 })
